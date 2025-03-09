6fe1186696b2b01396580515cc459b1ba52d1819
Moataz1234 <moataza630@gmail.com>
2025-03-09 19:39:18 +0200

    secound_commit

--- src/pages/CategoryProducts/CategoryProducts.jsx
+++ src/pages/CategoryProducts/CategoryProducts.jsx
@@ -1,32 +1,34 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom"
-import catImg from "../../assets/imgs/categories/wok.png";
+import { useNavigate } from "react-router-dom"
+import NavHeader from "../../components/NavHeader/NavHeader";
+import { useCategories } from "../../store";
 
 export default function CategoryProducts() {
-    const params = useParams();
     const navigate = useNavigate();
     const [check, setCheck] = useState(false);
-
-    const [categories] = useState([
-        { name: "Cold Drinks", path: "cold", imgUrl: { catImg }, price: 500 },
-        { name: "Burgers", path: "burgers", imgUrl: { catImg }, price: 600 },
-        { name: "Pizza", path: "pizza", imgUrl: { catImg }, price: 500 },
-        { name: "Wok", path: "wok", imgUrl: { catImg }, price: 600 },
-        { name: "Desserts", path: "dessert", imgUrl: { catImg }, price: 650 },
-        { name: "Pasta", path: "pasta", imgUrl: { catImg }, price: 500 },
-    ]);
+    const { data: categories, resetActiveId, active_cat_id } = useCategories();
+    const [categoryInfo, setCategoryInfo] = useState({});
 
     useEffect(() => {
-        let obj = categories.find((el) => { return el.path == params.catName });
+        let obj = categories.find((el) => { return el.documentId == active_cat_id });
         if (obj) {
+            setCategoryInfo(obj)
             setCheck(true);
         } else {
             navigate("/error");
+        }
+        return () => {
+            // i will execute After Componenet UnMOunt
+            resetActiveId();
         }
         // eslint-disable-next-line
     }, []);
     // console.log(params);
     return (
-        check && <div> Products in {params.catName}</div>
+        check &&
+        <div>
+            <NavHeader tabName={categoryInfo.name} />
+            <h1>Products in Cat : {categoryInfo.name}</h1>
+        </div>
     )
 }
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,8 +3,8 @@ import styles from "./Dashboard.module.css";
 export default function Dashboard() {
 
     return (
-        <div className="bg-danger h-100 flex-grow-1" id={styles.Dashboard}>
-            
+        <div className="h-100 flex-grow-1" id={styles.Dashboard}>
+            <h1>Dashboard Page</h1>
         </div>
     )
 }
